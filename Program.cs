732d580591329beddf03c79f142f401f12778f3e
VitinDenoyr <vitordenoyr@gmail.com>
2026-08-26 22:06:51 -0300

    chore: Merging

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using System.Text.Json.Nodes;
+using System.Net.Mail;
 using System.Text.Json;
+using System.Net;
 
 public class ApiManager {
     private static readonly HttpClient _client = new();
@@ -32,15 +34,49 @@ public class ApiManager {
         return currPrice;
     }
 }
-/*
+
+public class EmailManager {
+    private readonly string _host;
+    private readonly int _port;
+    private readonly string _user;
+    private readonly string _pass;
+    private readonly string _sender;
+    private readonly string _recipient;
+
+    public EmailManager(string host, int port, string user, string pass, string sender, string recipient) {
+        _host = host;
+        _port = port;
+        _user = user;
+        _pass = pass;
+        _sender = sender;
+        _recipient = recipient;
+    }
+
+    public async Task SendEmail(string subject, string body) {
+        using var smtpClient = new SmtpClient(_host, _port)
+        {
+            Credentials = new NetworkCredential(_user, _pass),
+            EnableSsl = true
+        };
+
+        using var message = new MailMessage(_sender, _recipient, subject, body);
+        await smtpClient.SendMailAsync(message);
+    }
+}
+
 class Program {
     static async Task Main(string[] args){
         var config = JsonNode.Parse(File.ReadAllText("config.json"));
-        string token = config?["ApiToken"]?.ToString() ?? "";
+
+        int port = (int)config?["SmtpPort"]!;
+        string host = (string)config?["SmtpHost"]!;
+        string user = (string)config?["SmtpUser"]!;
+        string pass = (string)config?["SmtpPass"]!;
+        string sender = (string)config?["SenderEmail"]!;
+        string recipient = (string)config?["RecipientEmail"]!;
+        string token = (string)config?["ApiToken"]!;
 
         var api = new ApiManager(token);
-
-        decimal price = await api.GetStockValue("PETR4");
-        Console.WriteLine($"Preço do PETR4: {price}");
+        var emailManager = new EmailManager(host, port, user, pass, sender, recipient);
     }
-}*/
+}
